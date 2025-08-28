97e519f419c32877a5ae544103fcf8da08d2c90d
Azeem yaqoob <yaqoobazeem740@gmail.com>
2025-08-28 22:48:57 +0500

    auth bug fixed

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,32 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  // These routes are publicly accessible and do not require authentication.
-  // Clerk's middleware will still run on them, allowing you to access auth state.
-  publicRoutes: [
-    "/",
-    "/contact",
-    "/about",
-    "/privacy",
-    "/terms",
-    "/services/personal-training",
-    "/services/yoga",
-    "/services/cycling",
-    "/services/nutrition",
-    "/services/strength-training",
-    "/services/zumba",
-    "/api/contact",
-  ],
+// Create a route matcher for all public routes
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/contact",
+  "/about",
+  "/privacy",
+  "/terms",
+  "/services/personal-training",
+  "/services/yoga",
+  "/services/cycling",
+  "/services/nutrition",
+  "/services/strength-training",
+  "/services/zumba",
+  "/api/contact",
+  "/sign-in(.*)", // The sign-in page and all its sub-routes are public
+  "/sign-up(.*)", // The sign-up page and all its sub-routes are public
+]);
+
+export default clerkMiddleware((auth, req) => {
+  // If the route is not public, then it is protected
+  if (!isPublicRoute(req)) {
+    auth().protect();
+  }
 });
 
 export const config = {
+  // The following matcher runs middleware on all routes
+  // except static assets.
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
